159645cab36731266d361069311459e9b6682f9a
Hesher1993 <Daredevillll49@yandex.ru>
2021-02-16 21:46:36 +0300

    master

---
+++ src/main/java/ru/geekbrains/hesher/servlet/WelcomeController.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.hesher.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+
+}
